f32315eb5f577044970091e022e673e6963a3b0a
Akanksha <akanksha.yugasa@gmail.com>
2024-10-06 19:07:17 +0530

    add new create button in edit gatpass page

--- app/Filament/Resources/GatePassResource/Pages/EditGatePass.php
+++ app/Filament/Resources/GatePassResource/Pages/EditGatePass.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\GatePassResource\Pages;
 
 use App\Filament\Resources\GatePassResource;
 use Filament\Actions;
+use Filament\Pages\Actions\ButtonAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditGatePass extends EditRecord
@@ -13,6 +14,10 @@ class EditGatePass extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            ButtonAction::make('create')
+                ->label('New gate pass')
+                ->url(fn() => $this->getResource()::getUrl('create')), // Redirect to create page
+                // ->icon('heroicon-o-plus'),
             Actions\DeleteAction::make(),
         ];
     }
